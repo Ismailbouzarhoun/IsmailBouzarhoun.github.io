d86d0ee6463194413687abbba82d7fa827eb9b38
Ismail Bouzarhoun <91149489+Ismailbouzarhoun@users.noreply.github.com>
2022-09-21 16:50:39 +0100

    fix error

--- index.js
+++ index.js
@@ -24,12 +24,12 @@ function closemenu(){
 }
 
 
-const scriptURL = 'https://script.google.com/macros/s/AKfycbx-o9oxptcdEcaauth-AJ53RU6AkX_ExAW_7VYikzQvvQDD5uz6vq8xLHS-fzDxsiRk/exec'
-  const form = document.forms['submit-to-google-sheet']
+const scriptURL = 'https://script.google.com/macros/s/AKfycbx-o9oxptcdEcaauth-AJ53RU6AkX_ExAW_7VYikzQvvQDD5uz6vq8xLHS-fzDxsiRk/exec';
+const form = document.forms['submit-to-google-sheet']
 
-  form.addEventListener('submit', e => {
+form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-      .then(response => console.log('Success!', response))
-      .catch(error => console.error('Error!', error.message))
-  })
+        .then(response => console.log('Success!', response))
+        .catch(error => console.error('Error!', error.message))
+    })
