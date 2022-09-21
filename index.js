c9ff0026e8ac4c8cc8051ecdaf0ef119a9738325
Ismail Bouzarhoun <91149489+Ismailbouzarhoun@users.noreply.github.com>
2022-09-21 17:24:04 +0100

    message success

--- index.js
+++ index.js
@@ -26,10 +26,17 @@ function closemenu(){
 
 const scriptURL = 'https://script.google.com/macros/s/AKfycbzbNcI-VbDNpneSP0c7bbqNL-Gyp6yw5V-BQKlA5NjnIwRFDHFmRrok_4KuA1VLEI9A/exec';
 const form = document.forms['submit-to-google-sheet']
+const msg = document.getElementById('msg')
 
 form.addEventListener('submit', e => {
     e.preventDefault()
     fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-        .then(response => console.log('Success!', response))
+        .then(response =>{
+            msg.innerHTML = 'Message sent successfully'
+            setTimeout(function(){
+                msg.innerHTML = ''
+            },5000)
+            form.reset()
+        } )
         .catch(error => console.error('Error!', error.message))
     })
